c801e3b12419ab216c9edbc04221fc4666aa7f51
Henok <henokaragaw17@gmail.com>
2025-03-07 13:28:01 -0900

    fix api endpoint bug

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -4,50 +4,50 @@ import Post from "@/lib/models/Post";
 
 //GET:Retrieve a single post by id
 export async function GET(
-    request:Request,{params}:{params:{id:string}}
+    request:Request,{params}:{params: Promise<{id:string}>}
 ) {
   try {
     await connectToDB();
-    const post = await Post.findById(params.id);
+    const post = await Post.findById((await params).id);
 
     if(!post){
         return NextResponse.json({ success: false, message: 'Post not found' }, { status: 404 });  
     }
     return NextResponse.json({success:true, data:post},{status:201});
   } catch (error:any) {
-    return NextResponse.json({ success: false, error: error.message }, { status: 400 });
+    return NextResponse.json({ success: false, error: error.message }, { status: 500 });
   }
 }
 
 // PUT: Update a post (for editing content or incrementing likes)
-export async function PUT(request:Request,{params}:{params:{id:string}}){
+export async function PUT(request:Request,{params}:{params: Promise<{id:string}>}){
     try {
       await connectToDB();
       const body = await request.json();
-      const post = await Post.findByIdAndUpdate(params.id, body, {new:true});
+      const post = await Post.findByIdAndUpdate((await params).id, body, {new:true});
       
       if(!post){
         return NextResponse.json({ success: false, message: 'Post not found' }, { status: 404 });
       }
       return NextResponse.json({ success: true, data: post });
     } catch (error:any) {
-        return NextResponse.json({ success: false, error: error.message }, { status: 400 });   
+        return NextResponse.json({ success: false, error: error.message }, { status: 500 });   
     }
 }
 
 // DELETE: Remove a post by 
 export async function DELETE(
-    request:Request,{params}:{params:{id:string}}
+    request:Request,{params}:{params: Promise<{id:string}>}
 ){
     try {
       await connectToDB();
-      const post = await Post.findByIdAndDelete(params.id);
+      const post = await Post.findByIdAndDelete((await params).id);
       
       if(!post){
         return NextResponse.json({ success: false, message: 'Post not found' }, { status: 404 });
       }
       return NextResponse.json({success:true, data:{}});
     } catch (error:any) {
-        return NextResponse.json({ success: false, error: error.message }, { status: 400 });   
+        return NextResponse.json({ success: false, error: error.message }, { status: 500 });   
     }
 }
